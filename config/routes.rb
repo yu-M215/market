494e2596844aabbb15130623e44341c4c5292d0e
yu-M215 <yuu0609km.215@gmail.com>
2022-01-05 08:56:57 +0000

    [Add] Models&controllers&views about devise created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # ユーザー用
+  # URL /users/sign_in ...
+  devise_for :users,skip: [:passwords,], controllers: {
+    registrations: "publics/registrations",
+    sessions: 'publics/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+    sessions: "admins/sessions"
+  }
 end
