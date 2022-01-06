aed59cb92f81128d27fb698fe6e95228bfb034ed
yu-M215 <yuu0609km.215@gmail.com>
2022-01-06 07:45:06 +0000

    [Update] actions added users_controller

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,5 +1,19 @@
 class Public::UsersController < ApplicationController
+  before_action :set_user
+
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @user.save
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
   end
 end
