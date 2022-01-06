5a5af480577ab7c447ee947b1143519383b37156
yu-M215 <yuu0609km.215@gmail.com>
2022-01-06 07:43:08 +0000

    [Update] actions added products_controller

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,30 +1,40 @@
 class Public::ProductsController < ApplicationController
+
+  before_action :set_product, only:[:show,:edit,:update,:destroy]
+
   def index
     @products = Product.all
   end
-  
+
   def show
-    @product = Product.find(params[:id])
   end
-  
+
   def new
     @product = Product.new
   end
-  
+
   def create
+    @product = Product.new(product_params)
+    @product.save
   end
-  
+
   def edit
   end
-  
+
   def update
+    @product.update
   end
-  
+
   def destroy
+    @product.destroy
   end
-  
+
   private
-  
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
   def product_params
   end
 end
